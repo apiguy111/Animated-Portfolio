b35884d03941e657868607e406678aea79b38503
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2024-04-20 14:49:24 +0530

    Update page.jsx  in title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,7 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Transforming Vision into Seamless Experiences.{" "}
+            Transforming Vision into Seamless Experiences.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
